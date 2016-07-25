b2a4f42860f0cfae5c2672254021dc01c634c200
R. Terrado <r.terrado@gmail.com>
2016-07-25 13:42:52 -0700

    Basic functionality completed

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     // STEP 1 : Populate the pantry 
 
     var pantry = {
-        strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-        salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-        bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-        sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-        fruity: ['Slice of orange', 'dash of cassis', 'cherry on top'],
+        strong: ['glug of rum', 'slug of whisky', 'splash of gin'],
+        salty: ['olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+        bitter: ['shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+        sweet: ['sugar cube', 'spoonful of honey', 'splash of cola'],
+        fruity: ['slice of orange', 'dash of cassis', 'cherry on top'],
     };
 
     //step 2 : make constructor to fetch yes/no values
@@ -33,8 +33,7 @@ $(document).ready(function() {
             parseInt($('#drink-sweet input:checked').val()),
             parseInt($('#drink-fruity input:checked').val()));
         var ingredients = ingredientSelector(userPref);
-        console.log(userPref['strong']);
-
+        $('#finished-drink').text(ingredients);
     });
 
     //how to evaluate if it is yes or no? 
@@ -44,19 +43,35 @@ $(document).ready(function() {
 
     // userPref[0].bitter
 
-
+// Math.floor(Math.random() * 3)   
 
     function ingredientSelector(userPref) {
-    	var selectedIngredients = [];
+    	var finishedDrink = [];
+        var drinkOutput;
     	// / for in?
-        for (var i in userPref) {
-        	// i is the property --> for-in loop
-            if (userPref[i] === 1) {
 
-                // random get a [0] ingredient
-            }
+        for (var i in userPref) {
+            if (userPref[i] == 1) {
+                var ranNum = parseInt(Math.floor(Math.random() * 3));
+                finishedDrink.push(pantry[i][ranNum]);
+                }
         }
-    }
+        console.log(finishedDrink);
+        if (finishedDrink.length < 1) {
+            return 'Ye don\'t like nothin\'!? Git some water!'
+        } 
+        else if (finishedDrink.length == 1){
+            drinkOutput = finishedDrink.toString();
+            return drinkOutput;
+        }
+        else {
+            var lastItem = finishedDrink.pop();
+            drinkOutput = finishedDrink.join(', ') + ' and ' + lastItem;
+            return drinkOutput;
+        }
+           
+
+        }
 
 
 
